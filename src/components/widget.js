d3b1dcebfcf36c70cbc23e554b8546db52d8fd6c
Bhawana Dhamala <bhawanadhamala75@gmail.com>
2022-09-04 11:40:47 +0545

    first commit

---
+++ src/components/widget.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./widget.css";
+
+const Widget = () => {
+  
+
+  return (
+    <div className="container">
+        <div className="item item-1">Widget 1</div>
+        <div className="item item-2">Widget 2</div>
+        <div className="item item-3">Widget 3</div>
+        <div className="item item-4">Widget 4</div>
+        <div className="item item-5">Widget 5</div>
+        <div className="item item-6">Widget 6</div>
+    </div>
+
+  );
+
+};
+
+export default Widget
